2a6290e980748df5061b6a69e37e31f36afa96ca
Ax-Sch <axel.schmidt1@rwth-aachen.de>
2022-05-05 10:45:22 +0200

    removed old stuff

--- scripts/anonymize_ROH.sh
+++
@@ -1,9 +0,0 @@
-
-cat bedForROHAnalysis_ROH.hom.indiv | awk '{print "XXFID", "XXIID",$3,$4,$5,$6}' | head -n1 > annonymized_bedForROHAnalysis_ROH.hom.indiv
-
-cat bedForROHAnalysis_ROH.hom.indiv | awk '{print "XXFID", "XXIID",$3,$4,$5,$6}' | tail -n+2 | shuf -n200 >> annonymized_bedForROHAnalysis_ROH.hom.indiv
-
-cat bedForROHAnalysis_ROH.hom | sed "s/ * / /g" | awk '{print "XXFID", "XXIID",$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13}' | head -n1 > annonymized_bedForROHAnalysis_ROH.hom
-
-cat bedForROHAnalysis_ROH.hom | sed "s/ * / /g" | awk '{print "XXFID", "XXIID",$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13}' |  tail -n+2 | shuf -n2000  >> annonymized_bedForROHAnalysis_ROH.hom
-
